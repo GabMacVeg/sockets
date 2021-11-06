1790641cf4e27a1cd716aa2a8d40f4de3ded5f3e
Gabriel Alejandro Macedo Vega <gabriel.macedo.vega@gmail.com>
2021-11-05 21:33:59 -0600

    no le sabes

--- Principal.java
+++ Principal.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import Servidor.Controlador.CtrlPrincipal;
 
-public class Principal{
+public class Principal{//velez no le sabes
     public static void main(String[] args) {
         new CtrlPrincipal();
 
